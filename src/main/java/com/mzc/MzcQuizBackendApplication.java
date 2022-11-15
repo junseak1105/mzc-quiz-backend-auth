f13e450f5fe717df7a82d04c70b63ff9513ae8fa
yeongki0944 <yeongki0944@gmail.com>
2022-11-15 19:58:22 +0900

    init

---
+++ src/main/java/com/mzc/MzcQuizBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mzc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MzcQuizBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MzcQuizBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mzc/MzcQuizBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mzc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MzcQuizBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
