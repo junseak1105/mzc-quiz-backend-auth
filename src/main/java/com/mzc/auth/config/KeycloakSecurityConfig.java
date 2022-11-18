010fd62595d41aa7373216eed060a6d2131c6262
yeongki0944 <yeongki0944@gmail.com>
2022-11-19 00:46:22 +0900

    [Keycloak] feat: keycloak 환경 구축을 위한 테스트
    로컬 환경에서 키클록 서버 설치 후 Spring에서 RESTAPI를 통해 인증정보를 받아올 수 있는지 테스트를 진행했음.
    
    키클록과 서버 연결은 되었지만, 인증정보는 받아오지 못하는 상태
    
    application.properties파일에서 keycloak.ssl-required 관련되어서 설정 에러가 발생함.

---
+++ src/main/java/com/mzc/auth/config/KeycloakSecurityConfig.java
@@ -0,0 +1,46 @@
+package com.mzc.auth.config;
+
+import org.keycloak.adapters.springsecurity.KeycloakConfiguration;
+import org.keycloak.adapters.springsecurity.authentication.KeycloakAuthenticationProvider;
+import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
+import org.keycloak.adapters.springsecurity.filter.KeycloakAuthenticationProcessingFilter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper;
+import org.springframework.security.core.session.SessionRegistryImpl;
+import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
+import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
+
+@KeycloakConfiguration
+@EnableGlobalMethodSecurity(jsr250Enabled = true, prePostEnabled = true)
+public class KeycloakSecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        super.configure(http);
+
+        http
+                .authorizeRequests()
+                .antMatchers("/v1/auth/test/permitAll").permitAll()
+                .anyRequest().authenticated();
+        http.csrf().disable();
+    }
+
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception{
+        KeycloakAuthenticationProvider keycloakAuthenticationProvider = keycloakAuthenticationProvider();
+        keycloakAuthenticationProvider.setGrantedAuthoritiesMapper(new SimpleAuthorityMapper());
+        auth.authenticationProvider(keycloakAuthenticationProvider);
+    }
+
+    @Bean
+    @Override
+    protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
+        return new RegisterSessionAuthenticationStrategy(new SessionRegistryImpl());
+    }
+}
