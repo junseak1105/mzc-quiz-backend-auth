c16a6e83fb55403dacdc30a5be6f54aeb36ec8ff
yeongki0944 <yeongki0944@gmail.com>
2022-11-29 15:21:23 +0900

    [Play-Redis] config: update config
     * Webconfig, RedisConfig 수정

--- src/main/java/com/mzc/global/config/db/RedisConfig.java
+++ src/main/java/com/mzc/global/config/db/RedisConfig.java
@@ -1,28 +1,15 @@
 package com.mzc.global.config.db;
 
-/*import com.mzc.redis.model.QuizMessage;
-import com.mzc.redis.pub.MessagePublisher;
-import com.mzc.redis.pub.RedisMessagePublisher;
-import com.mzc.redis.sub.RedisMessageSubscriber;
-import org.springframework.beans.factory.annotation.Autowired;*/
+
 import org.springframework.beans.factory.annotation.Value;
-/*import org.springframework.context.annotation.Bean;*/
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-/*import org.springframework.data.redis.listener.ChannelTopic;
-import org.springframework.data.redis.listener.RedisMessageListenerContainer;
-import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
-import org.springframework.data.redis.serializer.GenericToStringSerializer;
-import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;*/
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
-import org.springframework.stereotype.Component;
-
-import java.util.List;
 
 @Configuration
 @EnableRedisRepositories
@@ -35,6 +22,8 @@ public class RedisConfig {
     @Value("${spring.redis.password}")
     private String redisPassword;
 
+
+    @Bean
     public RedisConnectionFactory redisConnectionFactory(){
         RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
         redisStandaloneConfiguration.setHostName(redisHost);
@@ -45,65 +34,15 @@ public class RedisConfig {
     }
 
     @Bean
-    public RedisTemplate<String, Object> redisTemplate(){
-        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+    public RedisTemplate<?, ?> redisTemplate(){
+        RedisTemplate<byte[], byte[]> redisTemplate = new RedisTemplate<>();
         redisTemplate.setConnectionFactory(redisConnectionFactory());
+
+        // RedisTemplate 사용할 때 직렬화, 역직렬화 방식이 JDK직렬화방식
         redisTemplate.setKeySerializer(new StringRedisSerializer());
         redisTemplate.setValueSerializer(new StringRedisSerializer());
         return redisTemplate;
     }
 
-    /*
-    @Autowired
-    private RedisConnectionFactory redisConnectionFactory;
-
-    @Bean
-    public RedisMessageListenerContainer container(RedisConnectionFactory redisConnectionFactory, MessageListenerAdapter messageListenerAdapter){
-        RedisMessageListenerContainer redisMessageListenerContainer = new RedisMessageListenerContainer();
-        redisMessageListenerContainer.setConnectionFactory(redisConnectionFactory);
-        redisMessageListenerContainer.addMessageListener(messageListenerAdapter,topic());
-        return redisMessageListenerContainer;
-    }
-
-    @Bean
-    MessageListenerAdapter messageListenerAdapter()
-    {
-        return new MessageListenerAdapter(new RedisMessageSubscriber(),"onMessage");
-    }
-    @Bean
-    public ChannelTopic topic() {
-        return new ChannelTopic("RedisQuiz");
-    }
-
-    @Bean
-    RedisTemplate<String,Object> redisTemplate(RedisConnectionFactory redisConnectionFactory)
-    {
-        RedisTemplate<String,Object> redisTemplate = new RedisTemplate<>();
-        redisTemplate.setConnectionFactory(redisConnectionFactory);
-        redisTemplate.setKeySerializer(new StringRedisSerializer());
-        redisTemplate.setValueSerializer(new GenericToStringSerializer<Object>(Object.class));
-        redisTemplate.setKeySerializer(new Jackson2JsonRedisSerializer<Object>(Object.class));
-        redisTemplate.setValueSerializer(new Jackson2JsonRedisSerializer<Object>(Object.class));
-        return redisTemplate;
-    }
-
-    @Bean
-    RedisTemplate<List,Object> listRedisTemplate(RedisConnectionFactory redisConnectionFactory)
-    {
-        RedisTemplate<List,Object> redisTemplate = new RedisTemplate<>();
-        redisTemplate.setConnectionFactory(redisConnectionFactory);
-        redisTemplate.setKeySerializer(new StringRedisSerializer());
-        redisTemplate.setValueSerializer(new GenericToStringSerializer<Object>(Object.class));
-        redisTemplate.setKeySerializer(new Jackson2JsonRedisSerializer<Object>(Object.class));
-        redisTemplate.setValueSerializer(new Jackson2JsonRedisSerializer<Object>(Object.class));
-        return redisTemplate;
-    }
-
-    @Bean
-    MessagePublisher messagePublisher()
-    {
-        return new RedisMessagePublisher(redisTemplate(redisConnectionFactory),topic());
-    }
-    */
 
 }
