ef608fb94031a7325f523db6e4a31beeedcb1cd7
yeongki0944 <yeongki0944@gmail.com>
2022-11-29 15:30:13 +0900

    [Play-Redis] feat: update Host and Client

---
+++ src/main/java/com/mzc/quiz/play/controller/HostController.java
@@ -0,0 +1,27 @@
+package com.mzc.quiz.play.controller;
+
+import com.mzc.global.Response.DefaultRes;
+import com.mzc.quiz.play.service.HostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HostController {
+
+    @Autowired
+    private HostService hostService;
+
+    @PostMapping("/v1/host/createPlay")
+    public DefaultRes createPlay(){
+        return hostService.createPlay();
+    }
+
+}
+
+// JWT를 통해서 SSO 로그인 인증
+// username을 -> MongoDB 조회
+// Redis 저장공간 생성
+// 1. 유저리스트 저장
+// 2. 문제별 정보 저장 -> Hash로 해도 될려나
+// 3.
