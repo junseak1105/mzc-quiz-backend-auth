6e7db9c7678a4e4731f2c210953fadc2de69a973
yeongki0944 <yeongki0944@gmail.com>
2022-12-03 11:25:33 +0900

    [Play] feat: test 특정 유저에게만 Websocket 메시지 전송 테스트

--- src/main/java/com/mzc/quiz/play/controller/Test_WS_RedisController.java
+++ src/main/java/com/mzc/quiz/play/controller/Test_WS_RedisController.java
@@ -1,12 +1,16 @@
 package com.mzc.quiz.play.controller;
 
+import com.mzc.quiz.play.model.QuizMessage;
 import com.mzc.quiz.play.service.Test_WS_RedisService;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.messaging.simp.annotation.SendToUser;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
 
 @RestController
 @Log4j2
@@ -18,11 +22,12 @@ public class Test_WS_RedisController {
     @Autowired
     private SimpMessagingTemplate simpMessagingTemplate;
 
+
     @MessageMapping("/session/test")
-    @SendToUser("/queue/session")
-    public String sesionTest(){
-        log.info("session/Test");
-        return "private message";
+    public void sessionSampleCode(@RequestBody QuizMessage quizMessage, Principal principal){
+        String user = principal.getName();
+        String retUrl = "/queue/"+quizMessage.getPinNum();
+        simpMessagingTemplate.convertAndSendToUser(user, retUrl, "123123123123");
     }
 
 }
